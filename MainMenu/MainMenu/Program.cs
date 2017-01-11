91df6b07e751428e68ed90abae1cdc702c0f2bfa
dtangram <dtangram@yahoo.com>
2017-01-11 16:08:49 -0600

    Added MainMenu folder for C# practice.

---
+++ MainMenu/MainMenu/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Display the menu and prompt the user to enter the menu item they want to use.
+            Console.WriteLine("Please choose the menu item you need by typing it and pressing enter.\r\n1. SwapName  2. Backwards  3.AgeConvert  4. TempConvert");
+        }
+    }
+}
